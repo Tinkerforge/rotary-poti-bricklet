b13fff3984a5b51e64b72d6fbf6648865efd9344
Olaf Lüke <olaf@tinkerforge.com>
2011-10-06 19:29:28 +0200

    add firmware version and name

--- software/src/config.h
+++ software/src/config.h
@@ -28,6 +28,11 @@
 #include "bricklib/drivers/board/sam3s/SAM3S.h"
 #include "poti.h"
 
+#define BRICKLET_HARDWARE_NAME "Rotary Poti Bricklet 1.0"
+#define BRICKLET_FIRMWARE_VERSION_MAJOR 1
+#define BRICKLET_FIRMWARE_VERSION_MINOR 0
+#define BRICKLET_FIRMWARE_VERSION_REVISION 0
+
 #define LOGGING_LEVEL LOGGING_DEBUG
 #define DEBUG_BRICKLET 0
 
