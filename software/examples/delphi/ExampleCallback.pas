6b98fa012cee533c4f9ac7e74e9860e4f6442656
Matthias Bolte <matthias@tinkerforge.com>
2012-07-25 13:34:47 +0200

    Add Delphi examples

---
+++ software/examples/delphi/ExampleCallback.pas
@@ -0,0 +1,62 @@
+program ExampleCallback;
+
+{$ifdef MSWINDOWS}{$apptype CONSOLE}{$endif}
+{$ifdef FPC}{$mode OBJFPC}{$H+}{$endif}
+
+uses
+  SysUtils, IPConnection, BrickletRotaryPoti;
+
+type
+  TExample = class
+  private
+    ipcon: TIPConnection;
+    rp: TBrickletRotaryPoti;
+  public
+    procedure PositionCB(const position: smallint);
+    procedure Execute;
+  end;
+
+const
+  HOST = 'localhost';
+  PORT = 4223;
+  UID = '9Hh';
+
+var
+  e: TExample;
+
+{ Callback function for position callback (parameter has range 0-100) }
+procedure TExample.PositionCB(const position: smallint);
+begin
+  WriteLn(Format('Position: %d', [position]));
+end;
+
+procedure TExample.Execute;
+begin
+  { Create IP connection to brickd }
+  ipcon := TIPConnection.Create(HOST, PORT);
+
+  { Create device object }
+  rp := TBrickletRotaryPoti.Create(UID);
+
+  { Add device to IP connection }
+  ipcon.AddDevice(rp);
+  { Don't use device before it is added to a connection }
+
+  { Set Period for position callback to 0.05s (50ms)
+    Note: The position callback is only called every 50ms if the
+          position has changed since the last call! }
+  rp.SetPositionCallbackPeriod(50);
+
+  { Register position callback to procedure PositionCB }
+  rp.OnPosition := {$ifdef FPC}@{$endif}PositionCB;
+
+  WriteLn('Press key to exit');
+  ReadLn;
+  ipcon.Destroy;
+end;
+
+begin
+  e := TExample.Create;
+  e.Execute;
+  e.Destroy;
+end.
---
+++ software/examples/delphi/ExampleSimple.pas
@@ -0,0 +1,52 @@
+program ExampleSimple;
+
+{$ifdef MSWINDOWS}{$apptype CONSOLE}{$endif}
+{$ifdef FPC}{$mode OBJFPC}{$H+}{$endif}
+
+uses
+  SysUtils, IPConnection, BrickletRotaryPoti;
+
+type
+  TExample = class
+  private
+    ipcon: TIPConnection;
+    rp: TBrickletRotaryPoti;
+  public
+    procedure Execute;
+  end;
+
+const
+  HOST = 'localhost';
+  PORT = 4223;
+  UID = '9Hh';
+
+var
+  e: TExample;
+
+procedure TExample.Execute;
+var position: smallint;
+begin
+  { Create IP connection to brickd }
+  ipcon := TIPConnection.Create(HOST, PORT);
+
+  { Create device object }
+  rp := TBrickletRotaryPoti.Create(UID);
+
+  { Add device to IP connection }
+  ipcon.AddDevice(rp);
+  { Don't use device before it is added to a connection }
+
+  { Get current position (value has range 0-100) }
+  position := rp.GetPosition;
+  WriteLn(Format('Position: %d', [position]));
+
+  WriteLn('Press key to exit');
+  ReadLn;
+  ipcon.Destroy;
+end;
+
+begin
+  e := TExample.Create;
+  e.Execute;
+  e.Destroy;
+end.
