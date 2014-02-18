ac4e11513feff3ec8a4939c69dc14e7553866ce1
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-02-18 16:09:11 +0100

    javascript: examples fixed

--- software/examples/javascript/ExampleCallback.js
+++ software/examples/javascript/ExampleCallback.js
@@ -1,12 +1,11 @@
-var IPConnection = require('Tinkerforge/IPConnection');
-var BrickletRotaryPoti = require('Tinkerforge/BrickletRotaryPoti');
+var Tinkerforge = require('tinkerforge');
 
 var HOST = 'localhost';
 var PORT = 4223;
 var UID = 'ek5';// Change to your UID
 
-var ipcon = new IPConnection();// Create IP connection
-var poti = new BrickletRotaryPoti(UID, ipcon);// Create device object
+var ipcon = new Tinkerforge.IPConnection();// Create IP connection
+var poti = new Tinkerforge.BrickletRotaryPoti(UID, ipcon);// Create device object
 
 ipcon.connect(HOST, PORT,
     function(error) {
@@ -15,7 +14,7 @@ ipcon.connect(HOST, PORT,
 );// Connect to brickd
 
 // Don't use device before ipcon is connected
-ipcon.on(IPConnection.CALLBACK_CONNECTED,
+ipcon.on(Tinkerforge.IPConnection.CALLBACK_CONNECTED,
     function(connectReason) {
         // Set Period for position callback to 0.05s (50ms)
         // Note: The position callback is only called every 50ms if the 
@@ -25,7 +24,7 @@ ipcon.on(IPConnection.CALLBACK_CONNECTED,
 );
 
 // Register position callback
-poti.on(BrickletRotaryPoti.CALLBACK_POSITION,
+poti.on(Tinkerforge.BrickletRotaryPoti.CALLBACK_POSITION,
     // Callback function for position callback (parameter has range -150 to 150)
     function(position) {
         console.log('Position: '+position);
--- software/examples/javascript/ExampleSimple.js
+++ software/examples/javascript/ExampleSimple.js
@@ -1,12 +1,11 @@
-var IPConnection = require('Tinkerforge/IPConnection');
-var BrickletRotaryPoti = require('Tinkerforge/BrickletRotaryPoti');
+var Tinkerforge = require('tinkerforge');
 
 var HOST = 'localhost';
 var PORT = 4223;
 var UID = 'ek5';// Change to your UID
 
-var ipcon = new IPConnection();// Create IP connection
-var poti = new BrickletRotaryPoti(UID, ipcon);// Create device object
+var ipcon = new Tinkerforge.IPConnection();// Create IP connection
+var poti = new Tinkerforge.BrickletRotaryPoti(UID, ipcon);// Create device object
 
 ipcon.connect(HOST, PORT,
     function(error) {
@@ -15,7 +14,7 @@ ipcon.connect(HOST, PORT,
 );// Connect to brickd
 
 // Don't use device before ipcon is connected
-ipcon.on(IPConnection.CALLBACK_CONNECTED,
+ipcon.on(Tinkerforge.IPConnection.CALLBACK_CONNECTED,
     function(connectReason) {
         //Get current position of poti (return value has range -150 to 150)
         poti.getPosition(
