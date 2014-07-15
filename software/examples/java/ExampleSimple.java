5917541ba007609b0fef59ce1012f6647af60781
Matthias Bolte <matthias@tinkerforge.com>
2014-07-15 14:30:11 +0200

    java: Replace System.console() usage with an Eclipse compatible pattern

--- software/examples/java/ExampleSimple.java
+++ software/examples/java/ExampleSimple.java
@@ -20,7 +20,7 @@ public class ExampleSimple {
 
 		System.out.println("Position: " + position);
 
-		System.console().readLine("Press key to exit\n");
+		System.out.println("Press key to exit"); System.in.read();
 		ipcon.disconnect();
 	}
 }
