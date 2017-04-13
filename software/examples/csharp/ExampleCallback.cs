c14ed62731f4f215a4a01d794aeca38ed14790a1
Matthias Bolte <matthias@tinkerforge.com>
2017-04-13 12:53:13 +0200

    csharp: Adapt example

--- software/examples/csharp/ExampleCallback.cs
+++ software/examples/csharp/ExampleCallback.cs
@@ -22,7 +22,7 @@ class Example
 		// Don't use device before ipcon is connected
 
 		// Register position callback to function PositionCB
-		rp.Position += PositionCB;
+		rp.PositionCallback += PositionCB;
 
 		// Set period for position callback to 0.05s (50ms)
 		// Note: The position callback is only called every 0.05 seconds
