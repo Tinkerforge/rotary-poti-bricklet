ec5fa6083b0a33045da6eda7a1a1106d89169113
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 16:27:01 +0100

    rust: Accept host names for ip connection, fix naming inconsistencies

--- software/examples/rust/example_callback.rs
+++ software/examples/rust/example_callback.rs
@@ -1,31 +1,32 @@
 use std::{error::Error, io, thread};
-use tinkerforge::{ipconnection::IpConnection, rotary_poti_bricklet::*};
+use tinkerforge::{ip_connection::IpConnection, rotary_poti_bricklet::*};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Rotary Poti Bricklet
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Rotary Poti Bricklet.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let rotary_poti_bricklet = RotaryPotiBricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let rp = RotaryPotiBricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
-    //Create listener for position events.
-    let position_listener = rotary_poti_bricklet.get_position_receiver();
-    // Spawn thread to handle received events. This thread ends when the rotary_poti_bricklet
+    // Create receiver for position events.
+    let position_receiver = rp.get_position_receiver();
+
+    // Spawn thread to handle received events. This thread ends when the `rp` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
-        for event in position_listener {
-            println!("Position: {}", event); // Range: -150 to 150
+        for position in position_receiver {
+            println!("Position: {}", position); // Range: -150 to 150
         }
     });
 
-    // Set period for position listener to 0.05s (50ms)
+    // Set period for position receiver to 0.05s (50ms).
     // Note: The position callback is only called every 0.05 seconds
     //       if the position has changed since the last call!
-    rotary_poti_bricklet.set_position_callback_period(50);
+    rp.set_position_callback_period(50);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
--- software/examples/rust/example_simple.rs
+++ software/examples/rust/example_simple.rs
@@ -1,20 +1,20 @@
 use std::{error::Error, io};
 
-use tinkerforge::{ipconnection::IpConnection, rotary_poti_bricklet::*};
+use tinkerforge::{ip_connection::IpConnection, rotary_poti_bricklet::*};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Rotary Poti Bricklet
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Rotary Poti Bricklet.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let rotary_poti_bricklet = RotaryPotiBricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let rp = RotaryPotiBricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
-    // Get current position
-    let position = rotary_poti_bricklet.get_position().recv()?;
+    // Get current position.
+    let position = rp.get_position().recv()?;
     println!("Position: {}", position); // Range: -150 to 150
 
     println!("Press enter to exit.");
