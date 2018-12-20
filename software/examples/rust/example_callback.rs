1840aadf7e552924b949cac31954bd91b8329717
Erik Fleckstein <erik@tinkerforge.com>
2018-12-20 16:40:08 +0100

    rust: Reformat examples.

--- software/examples/rust/example_callback.rs
+++ software/examples/rust/example_callback.rs
@@ -1,5 +1,8 @@
-use std::{error::Error, io, thread};
-use tinkerforge::{ip_connection::IpConnection, rotary_poti_bricklet::*};
+use std::{io, error::Error};
+use std::thread;
+use tinkerforge::{ip_connection::IpConnection, 
+                  rotary_poti_bricklet::*};
+
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -10,23 +13,23 @@ fn main() -> Result<(), Box<dyn Error>> {
     let rp = RotaryPotiBricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-                                          // Don't use device before ipcon is connected.
+    // Don't use device before ipcon is connected.
 
-    let position_receiver = rp.get_position_callback_receiver();
+     let position_receiver = rp.get_position_callback_receiver();
 
-    // Spawn thread to handle received callback messages.
-    // This thread ends when the `rp` object
-    // is dropped, so there is no need for manual cleanup.
-    thread::spawn(move || {
-        for position in position_receiver {
-            println!("Position: {}", position); // Range: -150 to 150
-        }
-    });
+        // Spawn thread to handle received callback messages. 
+        // This thread ends when the `rp` object
+        // is dropped, so there is no need for manual cleanup.
+        thread::spawn(move || {
+            for position in position_receiver {           
+                		println!("Position: {}", position); // Range: -150 to 150
+            }
+        });
 
-    // Set period for position receiver to 0.05s (50ms).
-    // Note: The position callback is only called every 0.05 seconds
-    //       if the position has changed since the last call!
-    rp.set_position_callback_period(50);
+		// Set period for position receiver to 0.05s (50ms).
+		// Note: The position callback is only called every 0.05 seconds
+		//       if the position has changed since the last call!
+		rp.set_position_callback_period(50);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
